a5727ed5d8e04615434b059facb50843d4cd69be
Zoruda <stronger6@gmail.com>
2020-02-20 16:07:49 -0700

    Created Game scene, map generation, created outpost prefabs, developed map scrolling, and outpost expansion/contraction.

---
+++ Assets/Scripts/OutpostManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6f07679eecbe58068be45f799b8373f3718e985576bd088c31db324b7912f50
+size 2222
---
+++ Assets/Scripts/WorldWrap.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c62b4057a410343a514cb4c5d51991fc615d845dab1c13e04b2bf0c8177d482c
+size 1104
