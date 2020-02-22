036a2f8391db242ced45d3def9cf632b29edc619
Quinn Bast <quinn.bast@hotmail.com>
2020-02-22 14:59:08 -0600

    add: sub spawning. Tried to fix background but not the best

--- Assets/Scripts/OutpostSpawn.cs
+++ Assets/Scripts/OutpostSpawn.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92395068dbfe43191006eba7a52b20b7fe3f0ea8ac93ce5714f68e7bf8203636
-size 3511
+oid sha256:76e2039b9c26dfa9d0b4a6a402c0533214373a4fd3e040a18e7bb6a4888a3a35
+size 3783
---
+++ Assets/Scripts/SubSpawn.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7cc83b17a93459879904bfef3d7fdd4b0b96f9f6d19ea3c18895ed3223de323c
+size 1557
