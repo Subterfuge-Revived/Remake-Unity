6209460b6671ab2fd7f7e4bc2810baf2f702f3f3
Quinn Bast <quinn.bast@hotmail.com>
2020-02-26 21:05:58 -0600

    combats and player colors

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39c7982bbfa106e02ddce684362335d1ab398640f51d86104dda3eb30d17eb7e
+oid sha256:3ceee32d5ab9374f45a61d42257f28f5f9ddcd128669bdf58cb566e3e0d551c7
 size 4355
