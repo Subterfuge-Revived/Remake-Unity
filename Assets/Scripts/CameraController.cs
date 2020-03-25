6720be3f2a3b059960f9cfe1282c2c259de32903
Xutong Wang <TW2000x@gmail.com>
2020-03-25 05:29:55 +0000

    Map panning now stays under mouse

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ceee32d5ab9374f45a61d42257f28f5f9ddcd128669bdf58cb566e3e0d551c7
-size 4355
+oid sha256:72d314cbdb26b50cf6f021bde47b3b46bad26b77c33ebb1a670108b0d0fd267a
+size 4491
