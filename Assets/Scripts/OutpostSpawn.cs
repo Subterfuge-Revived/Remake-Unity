c1db726aa2409f692d794a41c96b9f73d276af53
Xutong Wang <TW2000x@gmail.com>
2020-03-25 05:29:55 +0000

    Map panning now stays under mouse

--- Assets/Scripts/OutpostSpawn.cs
+++ Assets/Scripts/OutpostSpawn.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ce340eda64394beda1d2ea8fc98ddc43be87b6e0ea2001a9b74889daee7c059
-size 4264
+oid sha256:38b79b498e4aba701586f25e98a88d4bb5d221ac13b29af26a9ca62672f26fdc
+size 4389
