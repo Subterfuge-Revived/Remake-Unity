c9be6c365e60d4267e221f5ef77684582b736914
Xutong Wang <TW2000x@gmail.com>
2020-03-27 08:11:18 +0000

    Rewrote map panning in terms of mouse click, mouse hold, and mouse release. Allowed panning to continue when mouse goes over other UI elements. Tidied up code.

--- Assets/Scripts/Game/OutpostSpawn.cs
+++ Assets/Scripts/Game/OutpostSpawn.cs
@@ -23,6 +23,7 @@ public class OutpostSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         // Get the gameroom from the applications state
         GameRoom room = ApplicationState.currentGameRoom;
         
@@ -46,10 +47,11 @@ public class OutpostSpawn : MonoBehaviour
         config.maxiumumOutpostDistance = 100;
         config.minimumOutpostDistance = 30;
         config.outpostsPerPlayer = 5;
+        */
         
         //////// GENERATE DUMMY PLAYERS ////////
         // (comment out above code)
-        /*
+        
         players.Add(new Player(1));
         players.Add(new Player(2));
 
@@ -59,7 +61,7 @@ public class OutpostSpawn : MonoBehaviour
         config.maxiumumOutpostDistance = 100;
         config.minimumOutpostDistance = 30;
         config.outpostsPerPlayer = 5; 
-        */
+        
         
         Game server = new Game(config);
         List<Outpost> outposts;
