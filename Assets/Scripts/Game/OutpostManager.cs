1d088b59437c46c5699e3beb7cd7f4a7bab12fda
Quinn Bast <quinn.bast@hotmail.com>
2020-03-27 15:42:32 -0600

    fixed player colors when network loading

--- Assets/Scripts/Game/OutpostManager.cs
+++ Assets/Scripts/Game/OutpostManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:073259d8a75c23ee5ff0d6a766ec61d113626b25dc77a7fab5b0d90975850979
-size 3344
+oid sha256:07d35249334c97309cb4cadfd9ad4d4a8a5b4d4d7c0ac6fa182aa86037ff5faa
+size 3390
