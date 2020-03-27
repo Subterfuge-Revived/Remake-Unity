c90788cbfbae483c93d93c95609ecde0e0f30314
Quinn Bast <quinn.bast@hotmail.com>
2020-03-27 15:42:32 -0600

    fixed player colors when network loading

--- Assets/Scripts/Game/SubManager.cs
+++ Assets/Scripts/Game/SubManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e34c3b352f5b16d2563b8a25694189c03a984bac3d986df62850354202a28fc
-size 2981
+oid sha256:b9d148de627b6ee7f257a4a42843fb1f86c5c9c7fad54d5ba737ffd3bdd3ea1c
+size 3027
