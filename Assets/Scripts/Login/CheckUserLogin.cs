dc207bd140609370d3b989b9ba37e25815bcc1cb
Quinn Bast <quinn.bast@hotmail.com>
2020-04-05 23:31:19 -0600

    Moved API to the core dll

--- Assets/Scripts/Login/CheckUserLogin.cs
+++ Assets/Scripts/Login/CheckUserLogin.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d929f2d9c78a6c758cbc0a40537c35c3fe6362b9599a2b9f74c95ec82788957b
-size 1564
+oid sha256:b79633f6425c40255cfb94e1c2dc4b647e997f37abde1b2e388eab2d7a6d9441
+size 2232
