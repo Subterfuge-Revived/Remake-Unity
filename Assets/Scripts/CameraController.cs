ba84c025fc8bfcc627996eb4a593839d0d8be5f3
Xutong Wang <TW2000x@gmail.com>
2020-04-08 00:55:48 +0100

    Camera position is now wrapped in an RftVector, and better organised CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5472b98107d1c4f94a2b2b2cd56155807e725611df889522c87dfd59fd2c7244
-size 4878
+oid sha256:b22399cdbef866302b98dcbc44a6147f4e69e52c561fc0a299b5bde2b052e628
+size 4872
