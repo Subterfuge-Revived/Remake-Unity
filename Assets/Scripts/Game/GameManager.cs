b04b61df2a560bce4aea2bf2f8b718da90a2e067
Sebastian Clancy <foldit.com@gmail.com>
2020-04-17 14:38:42 -0700

    Fixed Time Machine Bugs

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bafa4c05c66f120b349a4c12293aab455c9e0d1628fe02e7a67f6b3570cfce7
-size 4463
+oid sha256:1bdec8683eb67d094f8d080954814cd96529bf5c4e8222e8722810ba3aab3138
+size 4417
