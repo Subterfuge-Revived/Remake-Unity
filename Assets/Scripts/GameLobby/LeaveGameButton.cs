1ba9fe4c27e18b729201f336d8971d4019d31ab8
Quinn Bast <quinn.bast@hotmail.com>
2020-04-21 13:49:59 -0600

    Updated networking api

--- Assets/Scripts/GameLobby/LeaveGameButton.cs
+++ Assets/Scripts/GameLobby/LeaveGameButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:daf7b328dd5f0f65fb31dfa8076b163b1f11c2f5250423575ebb54451172e75e
-size 1714
+oid sha256:23f65a6b9f5898a4038213e3e16c9f105ce1b1f27fb8011857fed23c3173c991
+size 1907
