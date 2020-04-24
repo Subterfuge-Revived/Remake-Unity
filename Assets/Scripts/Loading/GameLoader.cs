bbeabaa35363039a46cc6b87ce882244fc0a97c9
Quinn Bast <quinn.bast@hotmail.com>
2020-04-24 14:05:25 -0600

    Created a loading scene

---
+++ Assets/Scripts/Loading/GameLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Translation;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StringFactory.loadStrings();
+        SceneManager.LoadScene("LoginScreen");
+    }
+}
