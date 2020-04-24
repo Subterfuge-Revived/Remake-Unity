09316ecc7ad5c92d48a94d8000b4133f6b560c2e
Quinn Bast <quinn.bast@hotmail.com>
2020-04-23 22:10:48 -0600

    localization strings & class

--- Assets/Scripts/GameLobby/GameActionButton.cs
+++ Assets/Scripts/GameLobby/GameActionButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5efaaf8a97ef6efc4d05e2f164a85bc7a9300fb8c8c4ed646ed431c6a077b906
-size 3750
+oid sha256:e85d0a517921beb05de9390e86d2df7c225efde6b60a5eac7b4450cf9e5c945a
+size 3960
