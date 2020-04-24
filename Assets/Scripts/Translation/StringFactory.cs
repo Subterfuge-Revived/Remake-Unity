f869e24917029d323ebc0551f9eaa740b831ecfb
Quinn Bast <quinn.bast@hotmail.com>
2020-04-24 11:15:59 -0600

    Added default strings hard-coded if a network connection cannot be established.

--- Assets/Scripts/Translation/StringFactory.cs
+++ Assets/Scripts/Translation/StringFactory.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e349c328d1aa2b82f7feb78d8e183f99b08c9e933410b2fd2b4d6b9f1d10936
-size 664
+oid sha256:5a7f6f5cb3b14153ba6257cc43fa16394cc50c022e7c76d83aa218d71a1ada7b
+size 6216
