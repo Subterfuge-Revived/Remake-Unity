269651c7b9cf2685d06703fd286b3ad3c0a4cc7d
Quinn Bast <quinn.bast@hotmail.com>
2020-04-23 22:10:48 -0600

    localization strings & class

--- Assets/Scripts/GameLobby/LeaveGameButton.cs
+++ Assets/Scripts/GameLobby/LeaveGameButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23f65a6b9f5898a4038213e3e16c9f105ce1b1f27fb8011857fed23c3173c991
-size 1907
+oid sha256:a37bdf425f9aa25632155d9bfd0d2d793bb497c03f4e3bb4f7c615e4249d0272
+size 2025
