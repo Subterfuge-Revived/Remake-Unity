d1fcd7de36554ab739ab39e51005d19bc8b7a50f
Quinn Bast <quinn.bast@hotmail.com>
2020-05-22 15:46:29 -0600

    fix: subs dont appear backwards in time

--- Assets/Scripts/Login/CheckUserLogin.cs
+++ Assets/Scripts/Login/CheckUserLogin.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a63ee0e398e2b03ce988cf28cdc6f00c0e1a3e1a1006feec912a12772514eec
-size 2364
+oid sha256:4458d506a7e2f12a1497ec55c4520af851f027015a6fe0aedbcec4172806c1e9
+size 2339
