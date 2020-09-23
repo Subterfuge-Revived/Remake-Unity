b39cf342cff88f18fce41f6f7fd444419eff8f64
Quinn Bast <quinn.bast@hotmail.com>
2020-09-22 18:10:14 -0600

    Added UniWebView to project

---
+++ Assets/LocalFileLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalFileLoader : MonoBehaviour
+{
+    public UniWebView webGui;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        webGui.BackgroundColor = new Color(0,0,0,0);
+        string url = UniWebViewHelper.StreamingAssetURLForPath("local_www/index.html");
+        webGui.Load(url);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
