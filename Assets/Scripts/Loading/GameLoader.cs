14d66d89ed0457c3ea6063ab5fa6c9e9a8cb6e6f
Quinn Bast <quinn.bast@hotmail.com>
2020-09-28 19:09:00 -0600

    Updated to use unity json library

--- Assets/Scripts/Loading/GameLoader.cs
+++ Assets/Scripts/Loading/GameLoader.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b57bf72627a8b7e7852ceb12c9c54b3704033dd1729c7741de91a7ff1a3bf24f
-size 543
+oid sha256:748e8e107de1cccc9f007df9bffc467fac5308cbc13b1187a88486934b6dd28e
+size 544
