ef830110422c59da37076959d4132d0e17e5a5d8
Quinn Bast <quinn.bast@hotmail.com>
2021-02-13 13:42:54 -0600

    remove extra network connection

--- Assets/Rooms/Multiplayer/LoadMultiplayer/LoadMultiplayer.cs
+++ Assets/Rooms/Multiplayer/LoadMultiplayer/LoadMultiplayer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f54403efd906e3f638830d74f6e18e13eacaa48cdb43ebbba87e3d6ac17ee68
-size 1849
+oid sha256:4591141b50461539bddd70892f4fddc1b937bc0c59d89a1e6869744bee3082b3
+size 2371
