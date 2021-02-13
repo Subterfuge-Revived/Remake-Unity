e14a540adb620206933f8a20a820450eced0bb64
Quinn Bast <quinn.bast@hotmail.com>
2021-02-13 13:42:54 -0600

    remove extra network connection

--- Assets/Rooms/Multiplayer/LoadMultiplayer/LoadMultiplayer.cs
+++ Assets/Rooms/Multiplayer/LoadMultiplayer/LoadMultiplayer.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Grpc.Core;
+using SubterfugeCore.Core.Players;
 using SubterfugeRemakeService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,49 +12,57 @@ public class LoadMultiplayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        String Hostname = "52.14.116.178"; // For docker
-        // String Hostname = "localhost"; // For local
-        int Port = 5000;
-
-        var client = new SubterfugeClient.SubterfugeClient(Hostname, Port.ToString());
-        
-        // Ensure that the client can connect to the server.
-        try
+        if (ApplicationState.Client.isConnected)
         {
-            client.HealthCheck(new HealthCheckRequest());
-        }
-        catch (RpcException exception)
-        {
-            // Show error saying that the user cannot connect to the game servers.
-            SceneManager.LoadScene("Rooms/MainMenu/MainMenu");
-        }
-        
-        // If connected, verify user login:
-        var token = PlayerPrefs.GetString("token");
-        if (token != null)
-        {
+            var client = ApplicationState.Client.getClient();
+            // Ensure that the client can connect to the server.
             try
             {
-                var response = client.LoginWithToken(new AuthorizedTokenRequest()
+                client.HealthCheck(new HealthCheckRequest());
+            }
+            catch (RpcException exception)
+            {
+                // Show error saying that the user cannot connect to the game servers.
+                Debug.Log("Cannot connect to server.");
+                SceneManager.LoadScene("Rooms/MainMenu/MainMenu");
+            }
+            
+            // If connected, verify user login:
+            var token = PlayerPrefs.GetString("token");
+            if (token != null)
+            {
+                try
                 {
-                    Token = token,
-                });
-
-                if (response?.User?.Username != null)
+                    var response = client.LoginWithToken(new AuthorizedTokenRequest()
+                    {
+                        Token = token,
+                    });
+         
+                    if (response?.User?.Username != null)
+                    {
+                        // Take user to multiplayer lobby
+                        PlayerPrefs.SetString("username", response.User.Username);
+                        ApplicationState.player = new Player(response.User);
+                        SceneManager.LoadScene("GameSelect");
+                    }
+                }
+                catch (RpcException e)
                 {
-                    // Take user to multiplayer lobby
-                    PlayerPrefs.SetString("username", response.User.Username);
-                    SceneManager.LoadScene("GameSelect");
+                    // Unable to login with token, let the user login using their credentials instead.
+                    SceneManager.LoadScene("LoginScreen");
                 }
             }
-            catch (RpcException e)
+            else
             {
-                Debug.Log(e);
                 SceneManager.LoadScene("LoginScreen");
             }
         }
-        // Show player the login screen.
-        SceneManager.LoadScene("LoginScreen");
+        else
+        {
+            // Show error saying that the user cannot connect to the game servers.
+            Debug.Log("Cannot connect to server.");
+            SceneManager.LoadScene("Rooms/MainMenu/MainMenu");
+        }
     }
 
     // Update is called once per frame
