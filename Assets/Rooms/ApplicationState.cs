8d92369f0110801f09c9cf1192ce2079d292d1bb
Quinn Bast <quinn.bast@hotmail.com>
2021-05-14 00:02:34 -0600

    Revamp graphics and menus

--- Assets/Rooms/ApplicationState.cs
+++ Assets/Rooms/ApplicationState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e02198a12a4826b830a4100513c58e831b5cd744c32b2d71d674a39f262ecc8
-size 1350
+oid sha256:642f6985b4a610e3b43070c55a58a0f7784aed33468a2daaff3a31c2aec54ecb
+size 1339
--- Assets/Rooms/NetworkClient.cs
+++ Assets/Rooms/NetworkClient.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c42dff2ff808a717a0d53fa6d8126c6be2fe611196616e41821381b0996edfbe
-size 1181
+oid sha256:da1353b10b12a1608f102b56b804327f6aa8ff1d2bdc63128a25aa031a608e6a
+size 2033
